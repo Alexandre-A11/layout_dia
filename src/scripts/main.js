51205e31df3f18a5fa5600f3de35a0363fca9a89
Alexandre <alexandredd@duck.com>
2025-05-11 14:15:55 -0300

    Add header and nav section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const btnMenu = document.querySelector('.nav__menu-btn');
+const menuList = document.querySelector('.nav__menu-list');
+
+btnMenu.addEventListener('click', () => {
+  btnMenu.classList.toggle('is-open');
+  menuList.classList.toggle('is-open');
+});

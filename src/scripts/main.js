3290e7db95bf6afd8666ffcb845a5d85b1514345
Alexandre <alexandredd@duck.com>
2025-05-12 11:15:44 -0300

    Add home page section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,38 @@
 
 const btnMenu = document.querySelector('.nav__menu-btn');
 const menuList = document.querySelector('.nav__menu-list');
+const btnPrev = document.querySelector('.slideshow__btn--prev');
+const btnNext = document.querySelector('.slideshow__btn--next');
 
 btnMenu.addEventListener('click', () => {
   btnMenu.classList.toggle('is-open');
   menuList.classList.toggle('is-open');
 });
+
+const showSlides = (n) => {
+  let i;
+  let slides = document.querySelectorAll('.slideshow__slide');
+
+  if (slides.length === 0) return;
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+
+  slides[slideIndex - 1].style.display = 'block';
+};
+
+let slideIndex = 1;
+
+showSlides(slideIndex);
+
+btnPrev.addEventListener('click', () => showSlides((slideIndex += -1)));
+btnNext.addEventListener('click', () => showSlides((slideIndex += 1)));
